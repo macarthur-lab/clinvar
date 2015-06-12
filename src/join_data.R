7f88080c04d9e73cf598a14cab6f1e70d7888b5d
Eric Minikel <ericminikel@gmail.com>
2015-06-12 18:14:24 -0400

    temporarily added annotations of pathogenic and conflicted

--- src/join_data.R
+++ src/join_data.R
@@ -20,4 +20,12 @@ combined = merge(xml_extract, txt_extract, by='measureset_id')
 # re-order the columns
 combined = combined[,c('chrom','pos','ref','alt','mut','measureset_id','symbol','clinical_significance','review_status','hgvs_c','hgvs_p','all_pmids')]
 
+# add some layers of interpretation on top of this
+# note: we are trying to get the "overall" interpretation that is displayed in the upper right of the clinvar web pages but
+# it is not in any of the available FTP downloads, so this is a stopgap
+combined$pathogenic = as.integer(grepl('athogenic',combined$clinical_significance)) # 1 if at least one submission says path or likely path, 0 otherwise
+combined$conflicted = as.integer(grepl('athogenic',combined$clinical_significance) & grepl('enign',combined$clinical_significance)) # 1 if at least one submission each of [likely] benign and [likely] pathogenic
+
 write.table(combined,'clinvar_combined.tsv',sep='\t',row.names=F,col.names=T,quote=F)
+
+
