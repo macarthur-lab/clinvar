fb38eed0eb6748c8f1b9616080da81b6ed439906
Eric Minikel <ericminikel@gmail.com>
2015-07-08 17:51:00 -0400

    wget -N to avoid renames, and not assuming everything is executable

--- src/master.bash
+++ src/master.bash
@@ -14,7 +14,7 @@ wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/ClinVarFullRelease_00-latest.xml
 wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/variant_summary.txt.gz
 
 # extract the GRCh37 coordinates, mutant allele, MeasureSet ID and PubMed IDs from it
-./parse_clinvar_xml.py -x ClinVarFullRelease_00-latest.xml.gz -o clinvar_table_raw.tsv
+python parse_clinvar_xml.py -x ClinVarFullRelease_00-latest.xml.gz -o clinvar_table_raw.tsv
 # the above takes about 3 hours (with submitters + traits, old version was ~1h without), so recommend submitting as a job:
 # bsub -q priority -R rusage[mem=64] -oo cvxml.o -eo cvxml.e -J cvxml "./parse_clinvar_xml.py -x ClinVarFullRelease_00-latest.xml.gz -o clinvar_table_raw.tsv"
 
@@ -24,12 +24,12 @@ cat clinvar_table_raw.tsv | tail -n +2 | egrep -v "^[XYM]" | sort -k1,1n -k2,2n 
 cat clinvar_table_raw.tsv | tail -n +2 | egrep "^[XYM]" | sort -k1,1 -k2,2n -k3,3 -k4,4 >> clinvar_table_sorted.tsv # lexicographically sort non-numerical chroms at end
 
 # de-duplicate records
-./dedup_clinvar.py < clinvar_table_sorted.tsv > clinvar_table_dedup.tsv
+python dedup_clinvar.py < clinvar_table_sorted.tsv > clinvar_table_dedup.tsv
 
 # normalize (convert to minimal representation and left-align)
 # the normalization code is in a different repo (useful for more than just clinvar) so here I just wget it:
-wget https://raw.githubusercontent.com/ericminikel/minimal_representation/master/normalize.py
-./normalize.py -R $b37ref < clinvar_table_dedup.tsv > clinvar_table_dedup_normalized.tsv
+wget -N https://raw.githubusercontent.com/ericminikel/minimal_representation/master/normalize.py
+python normalize.py -R $b37ref < clinvar_table_dedup.tsv > clinvar_table_dedup_normalized.tsv
 
 # join information from the tab-delimited summary to the normalized genomic coordinates
 Rscript join_data.R
@@ -40,7 +40,7 @@ cat clinvar_combined.tsv | tail -n +2 | egrep -v "^[XYM]" | sort -k1,1n -k2,2n -
 cat clinvar_combined.tsv | tail -n +2 | egrep "^[XYM]" | sort -k1,1 -k2,2n -k3,3 -k4,4 >> clinvar_combined_sorted.tsv # lexicographically sort non-numerical chroms at end
 
 # now de-dup _again_, because the tab-delimited summary contains dups
-./dedup_clinvar.py < clinvar_combined_sorted.tsv > clinvar_combined_sorted_dedup.tsv
+python dedup_clinvar.py < clinvar_combined_sorted.tsv > clinvar_combined_sorted_dedup.tsv
 
 # create a text file
 cp clinvar_combined_sorted_dedup.tsv clinvar.tsv
