36a9a904f6c790e4f30f9280599daa8f4670dc43
bw2 <weisburd@broadinstitute.org>
2016-07-14 22:37:42 -0400

    print to stdout instead of to an output file. This allows piping into bgzip

--- src/clinvar_table_to_vcf.py
+++ src/clinvar_table_to_vcf.py
@@ -13,7 +13,7 @@ def gzopen(path, mode, verbose=True):
         return open(path, mode)
 
 
-def table_to_vcf(input_table_path, output_vcf_path=None):
+def table_to_vcf(input_table_path):
     # validate args
     if not os.path.isfile(input_table_path):
         sys.exit("ERROR: %s not found" % input_table_path)
@@ -25,13 +25,7 @@ def table_to_vcf(input_table_path, output_vcf_path=None):
     if missing_columns:
         sys.exit("ERROR: %s is missing columns: %s" % (input_table_path, str(missing_columns)))
 
-    if output_vcf_path is None:
-        output_vcf_path = input_table_path.replace(".tsv", "") + ".vcf.gz"
-
-    print("Writing output to %s" % output_vcf_path)
-
-    with gzopen(output_vcf_path, "w") as f:
-        f.write("""##source=ClinVar
+    print("""##source=ClinVar
 ##INFO=<ID=MUT,Number=1,Type=String,Description="MUT">
 ##INFO=<ID=MUT,Number=1,Type=String,Description="MEASURESET_ID">
 ##INFO=<ID=MUT,Number=1,Type=String,Description="SYMBOL">
@@ -41,48 +35,45 @@ def table_to_vcf(input_table_path, output_vcf_path=None):
 ##INFO=<ID=MUT,Number=1,Type=String,Description="ALL_TRAITS">
 ##INFO=<ID=MUT,Number=1,Type=String,Description="ALL_PMIDS">
 ##INFO=<ID=MUT,Number=1,Type=String,Description="ALL_PATHOGENIC">
-##INFO=<ID=MUT,Number=1,Type=String,Description="ALL_CONFLICTED">
-""")
-        f.write("\t".join(["#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"])+"\n")
-        for i, table_row in t.iterrows():
-            vcf_row = []
-            vcf_row.append(table_row["chrom"])
-            vcf_row.append(table_row["pos"])
-            vcf_row.append('.')  # ID
-            vcf_row.append(table_row["ref"])
-            vcf_row.append(table_row["alt"])
-            vcf_row.append('.')  # QUAL
-            vcf_row.append('.')  # FILTER
+##INFO=<ID=MUT,Number=1,Type=String,Description="ALL_CONFLICTED">""")
+    print("\t".join(["#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"]))
+    for i, table_row in t.iterrows():
+        vcf_row = []
+        vcf_row.append(table_row["chrom"])
+        vcf_row.append(table_row["pos"])
+        vcf_row.append('.')  # ID
+        vcf_row.append(table_row["ref"])
+        vcf_row.append(table_row["alt"])
+        vcf_row.append('.')  # QUAL
+        vcf_row.append('.')  # FILTER
 
-            info_field = collections.OrderedDict()
+        info_field = collections.OrderedDict()
 
-            # from VCF spec:
-            #    INFO - additional information: (String, no white-space, semi-colons, or equals-signs permitted; commas are
-            #    permitted only as delimiters for lists of values) INFO fields are encoded as a semicolon-separated series of short
-            #    keys with optional values in the format: <key>=<data>[,data].
-            for key in ["mut", "measureset_id", "symbol",
-                        "clinical_significance", "review_status",
-                        "all_submitters", "all_traits", "all_pmids",
-                        "pathogenic", "conflicted"]:
-                if pd.isnull(table_row[key]):
-                    continue
-                value = str(table_row[key])
-                value = re.sub('\s*[,;]\s*', '|', value)  # replace , or ; with |
-                value = value.replace("=", " eq ").replace(" ", "_")
+        # from VCF spec:
+        #    INFO - additional information: (String, no white-space, semi-colons, or equals-signs permitted; commas are
+        #    permitted only as delimiters for lists of values) INFO fields are encoded as a semicolon-separated series of short
+        #    keys with optional values in the format: <key>=<data>[,data].
+        for key in ["mut", "measureset_id", "symbol",
+                    "clinical_significance", "review_status",
+                    "all_submitters", "all_traits", "all_pmids",
+                    "pathogenic", "conflicted"]:
+            if pd.isnull(table_row[key]):
+                continue
+            value = str(table_row[key])
+            value = re.sub('\s*[,;]\s*', '|', value)  # replace , or ; with |
+            value = value.replace("=", " eq ").replace(" ", "_")
+            
+            info_field[key.upper()] = value
+        vcf_row.append(";".join([key+"="+value for key, value in info_field.items()]))
 
-                info_field[key.upper()] = value
-            vcf_row.append(";".join([key+"="+value for key, value in info_field.items()]))
+        print("\t".join(map(str, vcf_row)))
 
-            f.write("\t".join(map(str, vcf_row)) + "\n")
-            f.flush()
-
-    print("Done")
+    sys.stderr.write("Done\n")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input_table_path', help="Tab-delimited input table")
-    parser.add_argument('-o', '--output-vcf', help = "Output VCF file. [default: input_table_path.vcf.gz]")
     args = parser.parse_args()
 
-    table_to_vcf(args.input_table_path, args.output_vcf)
+    table_to_vcf(args.input_table_path)
